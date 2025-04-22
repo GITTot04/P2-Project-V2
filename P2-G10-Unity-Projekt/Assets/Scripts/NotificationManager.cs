9f91b28941064163d06aaea94aec85cb92e3281b
MeronKHZ <meronkba@gmail.com>
2025-04-22 13:44:45 +0200

    Notification

---
+++ P2-G10-Unity-Projekt/Assets/Scripts/NotificationManager.cs
@@ -0,0 +1,117 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+
+
+public class NotificationManager : MonoBehaviour
+{
+    public static NotificationManager Instance 
+    {
+        get
+        {
+            if(instance != null)
+            {
+                return instance;
+            }
+            instance = FindObjectOfType<NotificationManager>();
+
+            if(instance !=null)
+            {
+                return instance; 
+            }
+
+            CreateNewInstance(); 
+            {
+                return instance;
+            }
+        }
+    } // public Singleton instance of the NotificationManager class that can be accessed from anywhere 
+
+    public static NotificationManager CreateNewInstance() 
+    {
+        NotificationManager notificationManagerPrefab = Resources.Load<NotificationManager>("NotificationManager");
+        instance = Instantiate(notificationManagerPrefab);
+        return instance; // a public method to create a new instance of the NotificationManager class if one does not exist.
+    }
+
+   private static NotificationManager instance; //private Singleton instance to ensure the public singleton exists and Instance gets instance when refered. 
+
+   private void Awake()
+   {
+        if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+   }
+    
+    [SerializeField] private Button notificationButton; //Serializefield to allow the button to be set in the inspector and display the notififcation.
+    [SerializeField] private TextMeshProUGUI notificationText; //to display a text inside the button
+    [SerializeField] private float fadeTime; //to set the time it takes to fade out the notification on inspector
+    [SerializeField] private float slideDuration = 0.5f; // Duration of the slide animation
+    [SerializeField] private Vector2 offScreenPosition = new Vector2(0,500); //position off-scrren (above the screen)
+    [SerializeField] private Vector2 onScreenPosition = new Vector2(0, 0); //Final position on screen
+
+    private IEnumerator notificationCoroutine; //private IEnumerator to allow the notification to be displayed for a set amount of time. 
+    public void SetNewNotification(string message, string sceneName)
+    {
+        if(notificationCoroutine != null) //This will be true when a notifaction is currently fading out, so when it's active.
+        {
+            StopCoroutine(notificationCoroutine); //This will stop the current nofication from fading out if it's not null.
+        }
+
+        notificationButton.onClick.RemoveAllListeners(); //This will clear all the pervious listeners 
+        notificationButton.onClick.AddListener(() => LoadScene(sceneName));
+
+        notificationText.text = message; //set the the button text
+        notificationButton.gameObject.SetActive(true); //Show the button
+
+        //Start the slide-in animation
+        StartCoroutine(SlideInNotification());
+
+        notificationCoroutine = FadeOutNotification(); 
+        StartCoroutine(notificationCoroutine); //This will start the coroutine to display the notification. 
+    }
+
+        private IEnumerator SlideInNotification() 
+    {
+        RectTransform rectTransform = notificationButton.GetComponent<RectTransform>();
+        rectTransform.anchoredPosition = offScreenPosition; // off-screen position
+        float elapsedTime = 0f;
+        while (elapsedTime < slideDuration)
+        {
+            elapsedTime += Time.unscaledDeltaTime;
+            rectTransform.anchoredPosition = Vector2.Lerp(offScreenPosition, onScreenPosition, elapsedTime / slideDuration);
+            yield return null;
+        }
+
+        rectTransform.anchoredPosition = onScreenPosition; //To ensure it ends at the final position
+    }
+
+    private IEnumerator FadeOutNotification() 
+    {   
+        float t = 0;
+        Color initialColor = notificationButton.image.color;
+
+        while(t < fadeTime)
+        {
+            t += Time.unscaledDeltaTime;
+            float alpha = Mathf.Lerp(1f, 0f, t / fadeTime); 
+            notificationButton.image.color = new Color(initialColor.r, initialColor.g, initialColor.b, alpha);
+            notificationText.color = new Color(
+                notificationText.color.r, 
+                notificationText.color.g, 
+                notificationText.color.b,
+                alpha); 
+            yield return null; 
+        }
+        notificationButton.gameObject.SetActive(false);
+    }
+
+    private void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName); // Load the specified scene when the notification button is cliked
+    }
+}
---
+++ P2-G10-Unity-Projekt/Assets/Scripts/TestNotification.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class TestNotification : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        NotificationManager.Instance.SetNewNotification("This is a test notification", "NotificationTest2"); 
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time > 5f)
+        {
+            NotificationManager.Instance.SetNewNotification("Hey this is the second test notification <3", "NotificationTestScene3");
+            Destroy(this);
+        }        
+    }
+}
